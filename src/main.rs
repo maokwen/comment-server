11f4529049fa171d7a457adeca01315dece9b9e2
maokwen <18361388+maokwen@users.noreply.github.com>
2023-05-07 18:10:43 +0800

    add cors

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ extern crate rocket_sync_db_pools;
 #[cfg(test)]
 mod test;
 
-use rocket::fairing::AdHoc;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{AdHoc, Fairing, Info, Kind};
 use rocket::response::{status::Created, Debug};
 use rocket::serde::{json::Json, Deserialize, Serialize};
 use rocket::{Build, Rocket};
@@ -38,7 +40,7 @@ async fn post(db: Db, comment: Json<Comment>) -> Result<Created<Json<Comment>>> 
     let item = comment.clone();
 
     let now = Utc::now().naive_utc();
-    let offset = FixedOffset::east(8 * 3600);
+    let offset = FixedOffset::east_opt(8 * 3600).unwrap();
     let date = offset.from_utc_datetime(&now);
     let date_str = format!("{}", date.format("%Y-%m-%d %H:%M:%S"));
 
@@ -97,10 +99,30 @@ async fn init_db(rocket: Rocket<Build>) -> Rocket<Build> {
     rocket
 }
 
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::fairing())
         .attach(AdHoc::on_ignite("Rusqlite Init", init_db))
+        .attach(CORS)
         .mount("/", routes![post, list])
 }
